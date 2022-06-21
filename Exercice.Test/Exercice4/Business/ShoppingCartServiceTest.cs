2cf2958137b944fe3d014fd77522208c8649b9f9
eduardo-lopes-objective <eduardo.lopes@objective.com.br>
2022-06-21 14:38:38 +0000

    Ajustes para testar se a API foi acionada ou não e renomeado nome dos testes

--- Exercice.Test/Exercice4/Business/ShoppingCartServiceTest.cs
+++ Exercice.Test/Exercice4/Business/ShoppingCartServiceTest.cs
@@ -59,50 +59,68 @@ namespace Exercice.Test.Exercice4.Business
         #region Calculation of total test
 
         [Fact]
-        public async Task TotalMustBe38()
+        public async Task TotalGreatherThan100MustNotCallAPIWithResult150()
         {
-            List<ShoppingCartItem> shoppingCartItems = new(){
-                new(new Product("productA", 18), 2),
-                new(new Product("productB", 1), 2)
-            };
-
-            CalculateTotalCommand request = new();
-
-            request.ShoppingCart = new(new Customer("teste", 12345678), shoppingCartItems);
-
-            var result = await shoppingCartService.CalculateTotal(request);
-
-            Assert.NotNull(result);
-            Assert.Equal(result.ResponseTypeEnum, ResponseTypeEnum.Sucess);
-            Assert.True(result.Messages.Count == 0);
-            Assert.Equal(38, result.Total);
-        }
-
-        [Fact]
-        public async Task TotalMustBe38Dot5()
-        {
-            List<ShoppingCartItem> shoppingCartItems = new(){
-                new(new Product("productA", 18), 2),
-                new(new Product("productB", 1.25m), 2)
-            };
-
-            CalculateTotalCommand request = new();
-
-            request.ShoppingCart = new(new Customer("teste", 12345678), shoppingCartItems);
-
-            var result = await shoppingCartService.CalculateTotal(request);
-
-            Assert.NotNull(result);
-            Assert.Equal(result.ResponseTypeEnum, ResponseTypeEnum.Sucess);
-            Assert.True(result.Messages.Count == 0);
-            Assert.Equal(38.5m, result.Total);
-        }
-
-        [Fact]
-        public async Task TotalMustBe138()
-        {
+            var isAPICalled = false;
             correioAPIMock.Setup(it => it.CalculateFrete(It.IsNotNull<long>())).Returns((long value) =>
             {
+                isAPICalled = true;
+                return Task.FromResult(100);
+            });
+
+            List<ShoppingCartItem> shoppingCartItems = new(){
+                new(new Product("productA", 70), 2),
+                new(new Product("productB", 5), 2)
+            };
+
+            CalculateTotalCommand request = new();
+
+            request.ShoppingCart = new(new Customer("teste", 12345678), shoppingCartItems);
+
+            var result = await shoppingCartService.CalculateTotal(request);
+
+            Assert.NotNull(result);
+            Assert.Equal(result.ResponseTypeEnum, ResponseTypeEnum.Sucess);
+            Assert.True(result.Messages.Count == 0);
+            Assert.Equal(150, result.Total);
+            Assert.False(isAPICalled);
+        }
+
+        [Fact]
+        public async Task TotalGreatherThan100MustNotCallAPIWithResult150Dot5()
+        {
+            var isAPICalled = false;
+            correioAPIMock.Setup(it => it.CalculateFrete(It.IsNotNull<long>())).Returns((long value) =>
+            {
+                isAPICalled = true;
+                return Task.FromResult(100);
+            });
+
+            List<ShoppingCartItem> shoppingCartItems = new(){
+                new(new Product("productA", 70), 2),
+                new(new Product("productB", 5.25m), 2)
+            };
+
+            CalculateTotalCommand request = new();
+
+            request.ShoppingCart = new(new Customer("teste", 12345678), shoppingCartItems);
+
+            var result = await shoppingCartService.CalculateTotal(request);
+
+            Assert.NotNull(result);
+            Assert.Equal(result.ResponseTypeEnum, ResponseTypeEnum.Sucess);
+            Assert.True(result.Messages.Count == 0);
+            Assert.Equal(150.5m, result.Total);
+            Assert.False(isAPICalled);
+        }
+
+        [Fact]
+        public async Task TotalLowerThan100MustCallAPIWithResult138()
+        {
+            var isAPICalled = false;
+            correioAPIMock.Setup(it => it.CalculateFrete(It.IsNotNull<long>())).Returns((long value) =>
+            {
+                isAPICalled = true;
                 return Task.FromResult(100);
             });
 
@@ -121,9 +139,8 @@ namespace Exercice.Test.Exercice4.Business
             Assert.Equal(result.ResponseTypeEnum, ResponseTypeEnum.Sucess);
             Assert.True(result.Messages.Count == 0);
             Assert.Equal(138, result.Total);
+            Assert.True(isAPICalled);
         }
-
-
         #endregion
 
 
